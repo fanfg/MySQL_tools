a9013777c7fabd1055b3c955beabcad0d3e91b49
FanFuGui <fanfuguijiayou@163.com>
2018-01-17 16:55:25 +0800

    Update gethost.py
    modify something

--- gethost.py
+++ gethost.py
@@ -1,24 +1,38 @@
 
+
+
 #!/usr/bin/python
 import json
 import pymysql
-myurl = {"host":"monitor.db.ymatou.com", "port":30001, "user":"monitoruser","passwd":"mmowhci$53nZH2whc@452276w", "db":"dbmonitor", "charset":"UTF8"}
+
+
+myurl = {"host":"db.xxx.com", "port":1000, "user":"test","passwd":"123456", "db":"serverinfo", "charset":"UTF8"}
+
 def readmysql(sqlstr,myurl):
     myconn=pymysql.connect(host=myurl['host'],user=myurl['user'],passwd=myurl['passwd'],port=myurl['port'],db=myurl['db'],charset=myurl['charset'])
-    cur=myconn.cursor()
-    cur.execute(sqlstr)
-    sqlresult = cur.fetchall()
+    sqlresult=[]
+    try:
+        with myconn.cursor() as cur:
+            cur.execute(sqlstr)
+            sqlresult = cur.fetchall()
+    finally:
+        myconn.commit()
+        myconn.close()
     return sqlresult
-hostlist={}
-sql="SELECT  DISTINCT serverip FROM  t_mysqlserverinfo"
-ds=readmysql(sql,myurl)
-hostlist["mysql"]=[]
-for li in ds:
-    hostlist["mysql"].append(li[0])
-sql="SELECT DISTINCT  serverip FROM  t_mongoserverinfo"
-ds=readmysql(sql,myurl)
-hostlist["mongodb"]=[]
-for li in ds:
-    hostlist["mongodb"].append(li[0])
-print json.dumps(hostlist)
+
+if __name__== '__main__':
+    hostlist={}
+    sql="SELECT  DISTINCT serverip FROM  mysqlserverinfo;"
+    ds=readmysql(sql,myurl)
+    hostlist["mysql"]=[]
+    for li in ds:
+        hostlist["mysql"].append(li[0])
+    
+    sql="SELECT DISTINCT  serverip FROM  mongoserverinfo;"
+    ds=readmysql(sql,myurl)
+    hostlist["mongodb"]=[]
+    for li in ds:
+        hostlist["mongodb"].append(li[0])
+
+    print json.dumps(hostlist)                     
 
